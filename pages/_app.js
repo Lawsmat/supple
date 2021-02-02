96e7f6ebdc5c56e40e1840c9fbc1898b0abe378f
MoonBarc <39464198+MoonBarc@users.noreply.github.com>
2021-02-02 08:01:17 -0500

    Get nextjs ready for stripe (frontend)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from "@stripe/stripe-js";
+
 import '../styles/globals.css'
 
+const promise = loadStripe("<insert key>");
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Elements stripe={promise}>
+    <Component {...pageProps} />
+  </Elements>
 }
 
 export default MyApp
